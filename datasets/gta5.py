7fd5b7c9de379c51e7f15e39d658bd23ce19b83a
ariannalezzi <s347096@studenti.polito.it>
2025-04-16 15:25:56 +0200

    gta5.py

--- datasets/gta5.py
+++ datasets/gta5.py
@@ -1,22 +1,52 @@
+import os
+from PIL import Image
+import torch
 from torch.utils.data import Dataset
 
 # TODO: implement here your custom dataset class for GTA5
-
+#### VEDERE COME DIVIDERLO IN TRAIN E VAL (POI EVAL CON CITYSCAPES)
 
 class GTA5(Dataset):
-    def __init__(self):
+    def __init__(self, root, transform=None, target_transform=None):
         super(GTA5, self).__init__()
-        # TODO
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+        
+        # Image and label directories
+        self.image_dir = os.path.join(root, 'images')
+        self.label_dir = os.path.join(root, 'labels')
 
-        pass
+        # Collect sorted image and label
+        self.image_paths = sorted([
+            os.path.join(self.image_dir, f)     # Joins the folder path with the filename to get the full path to each image (/content/gta5/GTA5/images/00001.pn)
+            for f in os.listdir(self.image_dir) # LISTDIR -> lists all files in the images directory
+            if f.endswith('.png')               # Filters only files that are PNG images
+        ])
+        
+        # The same for the labels
+        self.label_paths = sorted([
+            os.path.join(self.label_dir, f)
+            for f in os.listdir(self.label_dir)
+            if f.endswith('.png')
+        ])
+
+        assert len(self.image_paths) == len(self.label_paths), \
+            f"Mismatch: {len(self.image_paths)} images vs {len(self.label_paths)} labels"
+
+        print(f"Loaded {len(self.image_paths)} GTA5 images.")
+
 
     def __getitem__(self, idx):
-        # TODO
+        img = Image.open(self.image_paths[idx]).convert('RGB')
+        label = Image.open(self.label_paths[idx])
 
-        pass
+        if self.transform:
+            img = self.transform(img)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return img, label
 
     def __len__(self):
-
-        # TODO
-
-        pass
+        return len(self.image_paths)
--- main.py
+++ main.py
@@ -45,3 +45,84 @@ optimizer = torch.optim.SGD(model.optim_parameters(base_lr), lr=base_lr, momentu
 for epoch in range(epochs):
     train_one_epoch(model, train_loader, optimizer, base_lr, epoch, epochs, device)
     best_miou, val_miou, ious = validate(model, val_loader, num_classes, device, best_miou)
+
+
+
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn as nn
+
+from datasets.cityscapes import CityScapes
+from models.deeplabv2.deeplabv2 import get_deeplab_v2
+from utils_p import *
+from train import *
+
+
+
+
+def main():
+    #Dataset definitions and transformation
+
+        # Define the transformations
+    transform = transforms.Compose([
+        transforms.Resize((512, 1024)),
+        transforms.ToTensor()
+    ])
+
+    target_transform = transforms.Compose([
+        transforms.Resize((512, 1024), interpolation=transforms.InterpolationMode.NEAREST),
+        transforms.PILToTensor()
+    ])
+
+    dataset_root = '/content/cityscapes/Cityscapes/Cityspaces' #meant to run on colab
+
+    train_dataset = CityScapes(root=dataset_root, split='train', transform=transform, target_transform=target_transform)
+    print(f"Train dataset size: {len(train_dataset)}")
+
+    test_dataset = CityScapes(root=dataset_root, split='val', transform=transform, target_transform=target_transform)
+    print(f"Test dataset size: {len(test_dataset)}")
+
+    from torch.utils.data import DataLoader
+
+    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=2, shuffle=True, num_workers=2)
+
+
+    #MODEL AND HYPERPARAMETERS
+    # Device setup
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Hyperparameters
+    num_classes = 19
+    base_lr = 2.5e-4
+    batch_size = 2
+    epochs = 50
+    crop_size = (512, 1024)
+
+    # Model
+    model = get_deeplab_v2(num_classes=num_classes, pretrain=True, pretrain_model_path='/content/drive/MyDrive/ML&DL/Progetto/MLDL_2025-main/deeplab_resnet_pretrained_imagenet.pth').to(device)
+    if torch.cuda.device_count() > 1:
+        print("Using", torch.cuda.device_count(), "GPUs")
+        model = nn.DataParallel(model)
+
+    # Define a loss function and optimizer
+    criterion = nn.CrossEntropyLoss()  # Assuming it's a segmentation task
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    best_miou = 0.0
+
+    # training loop
+    for epoch in range(epochs):
+        train_one_epoch(model, train_loader, optimizer, base_lr, epoch, epochs, device)
+
+    #validation
+    validate(model, test_loader, num_classes, device, best_miou)
+
+    #get statistics
+    flops, params = get_model_stats(model, input_shape=(3,512,1024), device = device)
+    latency = measure_latency(model, input_shape=(3,512,1024), device = device)
+
+    if __name__ == "main":
+        main()
